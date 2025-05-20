2cfc9576b4140aa4ba860d21a5082becb8e02392
Theo Kramer <kramer.theo@icloud.com>
2025-05-20 15:09:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hallo! Ich bin dein Bot auf Render 😊")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
